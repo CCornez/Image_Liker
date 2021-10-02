17463c0e84e4639c327091d3368511b4e2741269
CCornez <cyrilcornez@outlook.com>
2021-10-02 11:59:24 +0200

    access to database

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 import "../css/style.scss";
-import Square from "./classes/Square";
+import { httpClient } from "./helpers";
 
-new Square(20, 20, 200, document.querySelector(".test"));
-console.log(process.env.SECRET);
+const input = document.querySelector(".search form input");
+const form = document.querySelector(".search form");
+const sectionTemplate = document.querySelector(".sectionTemplate").innerHTML;
+const main = document.querySelector("main");
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  const { value } = input;
+  if (value.length > 3) {
+    httpClient(`/search/photos?query=${input.value}`).then((response) => {
+      main.innerHTML = response.data.results
+        .map((obj) => {
+          let description = obj.description;
+          const alt_description = obj.alt_description;
+          if ((obj.description = "null")) {
+            description = alt_description;
+          }
+          return sectionTemplate
+            .replace("#SECTION_IMAGE_URL", obj.urls.small)
+            .replace("#SECTION_TEXT", description)
+            .replace("#SECTION_IMAGE_ALT", alt_description);
+        })
+        .join("");
+    });
+  }
+  input.value = "";
+};
